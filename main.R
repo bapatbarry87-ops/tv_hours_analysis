e462e5cc571451a87063807ec9af9403380ac927
bapatbarry87-ops <bapatbarry87@gmail.com>
2026-01-21 10:50:56 +0000

    started analysis

---
+++ main.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(here)
+gss_cat
+
+tv_hours_table <- gss_cat %>%
+  group_by(marital) %>%
+  summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
+
+write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))
