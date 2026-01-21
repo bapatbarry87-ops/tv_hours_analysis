1b6678ebbd8c6049c0a491694c4ece29a2ab8cb5
bapatbarry87-ops <bapatbarry87@gmail.com>
2026-01-21 11:21:40 +0000

    filter to those 30 and ebove

--- main.R
+++ main.R
@@ -3,7 +3,8 @@ library(here)
 gss_cat
 
 tv_hours_table <- gss_cat %>%
+  filter(age >= 30) %>% 
   group_by(marital) %>%
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
-
+tv_hours_table
 write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))
