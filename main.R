16a01ab55a0499e49c886830e0d9d24c8fa94783
bapatbarry87-ops <bapatbarry87@gmail.com>
2026-01-22 06:51:36 +0000

    filter to keep only those under 30

--- main.R
+++ main.R
@@ -3,7 +3,7 @@ library(here)
 gss_cat
 
 tv_hours_table <- gss_cat %>%
-  filter(age >= 30) %>% 
+  filter(age < 30) %>% 
   group_by(marital) %>%
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 tv_hours_table
