88bb05f9fd93e15c2a8f5d95e6cc60a78727b178
bapatbarry87-ops <bapatbarry87@gmail.com>
2026-01-22 07:39:09 +0000

    make names longer

--- main.R
+++ main.R
@@ -2,9 +2,9 @@ library(tidyverse)
 library(here)
 gss_cat
 
-tv_hours_table <- gss_cat %>%
+tv_hours_tabulation <- gss_cat %>%
   filter(age < 30) %>% 
   group_by(marital) %>%
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
-tv_hours_table
-write_csv(tv_hours_table, here("tv_hours_by_marital.csv"))
+tv_hours_tabulation
+write_csv(tv_hours_tabulation, here("tv_hours_by_marital.csv"))
